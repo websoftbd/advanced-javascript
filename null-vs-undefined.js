25afb4289ec30081c1c9648425262d6a3482fb20
websoftbd <halimsha24@gmail.com.com>
2020-03-05 00:03:05 +0600

    double vs triple equal

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,17 +1,16 @@
-//let pakhi;
-//function add(num1. num2){
-//    console.log(num1 +num2);
-//  return
-// }
+let pakhi;
+function add(num1. num2){
+    console.log(num1 +num2);
+  return
+ }
 
-//function add(num1, num2){
-//  console.log(num1, num2);
-//}
+function add(num1, num2){
+  console.log(num1, num2);
+}
 
-//const premik ={name:"smart dude", phone:458421};
-//console.log(fun);
+const premik ={name:"smart dude", phone:458421};
+console.log(fun);
 
-//let fun = undefined;
-//consle.log(fun);
+let fun = undefined;
 let ages = [2, 5, 9]
 console.log(ages[11]);
