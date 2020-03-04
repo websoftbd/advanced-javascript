ccae6913456271443ed287d7670137e22c7fc6e1
websoftbd <halimsha24@gmail.com.com>
2020-03-04 23:21:06 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//falsy:
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//truthy
+//'0',''[]
+let name = 12;
+
+
+if (name || name == 0){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
