4998270e240cc7ccc11e0537e32ad642eaa8ad97
websoftbd <halimsha24@gmail.com.com>
2020-03-05 01:51:07 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+let bonus =20;
+
+function sum (first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fisky";
+        mood = 'funky';
+        mood = "cranky";
+        //console.log(mood);
+    }
+    console.log(day);
+    let day = "friday";
+    return result;
+}
+const output = sum(3, 7);
+//console.log(bonus)
